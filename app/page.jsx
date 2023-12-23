ea16a4c1491f73004c4189373ebb9273f3308bdf
Fazar Budiman <fazarbudiman1907@gmail.com>
2023-12-23 14:17:45 +0700

    change-all

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,5 @@
 import ContactList from "@/components/ContactList";
 
 export default function Home() {
-  // return <ContactList />;
-  return <div>Hello Fucking World</div>;
+  return <ContactList />;
 }
